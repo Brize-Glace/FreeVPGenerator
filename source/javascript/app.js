f79f18f47eec939c8bc4c049749efff05b1600b7
Brize Glace <viktorovitchromain@gmail.com>
2022-12-18 23:57:18 +0100

    0.2.0.0
    New interface
    Bug fixes

--- source/javascript/app.js
+++ source/javascript/app.js
@@ -8,12 +8,12 @@ var emptyText = document.getElementById("errorEmpty");
 
 console.log(monButton1)
 
-monButton1.addEventListener("click", function(){
-    
-    if (input.value === ""){
+monButton1.addEventListener("click", function () {
+
+    if (input.value === "") {
         console.log("Veuillez renseigner une valeur!")
         emptyText.style.display = "block"
-    } else{
+    } else {
         maDiv.style.display = "none";
         vpChoose.style.display = "block"
     }
@@ -21,17 +21,39 @@ monButton1.addEventListener("click", function(){
 
 var monButton2 = document.getElementById("bouton2")
 
-monButton2.addEventListener("click", function(){
+monButton2.addEventListener("click", function () {
     vpChoose.style.display = "none"
     formulaireCB.style.display = "block"
 })
 
-var cbSubmit =  document.getElementById("submitCB")
+var cbSubmit = document.getElementById("submitCB")
 var thanks = document.getElementById("thanks")
+var emptyCB = document.getElementById("emptyCB")
+const input2 = document.getElementById("nomCB")
+var input3 = document.getElementById("dateEXP")
+var input4 = document.getElementById("numero") 
+var input5 = document.getElementById("crypto")
 
-cbSubmit.addEventListener("click", function(){
-    formulaireCB.style.display = "none";
-    thanks.style.display = "block";
+cbSubmit.addEventListener("click", function () {
+    if (input2.value === "") {
+        console.log("Veuillez renseigner une valeur!")
+        emptyCB.style.display = "block"
+    } else if(input3.value === ""){
+        emptyCB.style.display = "block"
+    }  else if(input4.value === ""){
+        emptyCB.style.display = "block"
+    } else if(input5.value === ""){
+        emptyCB.style.display = "block"
+    } else {
+        formulaireCB.style.display = "none";
+        thanks.style.display = "block";
+    }
+})
+
+document.querySelectorAll('input[type="number"]').forEach(input =>{
+    input.oninput = () =>{
+        if(input.value.length > input.maxLength) input.value = input.value.slice(0, input.maxLength);
+    };
 })
 console.log(spin)
 
